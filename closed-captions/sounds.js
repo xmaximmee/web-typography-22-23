14068a70a20b320667842403e24b8fd871e7304b
xmaximmee <90137923+xmaximmee@users.noreply.github.com>
2023-05-22 17:37:02 +0200

    Add files via upload

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,20 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
+	0, //dit is sound0
 	9,
 	12,
 	15,
 	16.5,
 	24,
 	25.8,
-	31,
-	32,
-	34,
-	86
+	31, //sound8 1e tempo 10s
+	32, //sound9 2e tempo 10s
+	42, //sound9 3e tempo 10s
+	52, //sound10 4e tempo 10s
+	62, //sound11 5e tempo 5s
+	67, //sound12 5e tempo 7s
+	75,
+	86, //sound14
+	150.
 ];
